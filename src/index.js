6f1cfeae5c26a8f36304f2a5d128e10db54c5681
Joel John <johnjoel079@gmail.com>
2024-04-11 10:33:17 -0400

    code chcomplete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,78 @@
 // index.js
 
 // Callbacks
-const handleClick = (ramen) => {
-  // Add code
+const handleClick = (ramen) => { 
+  
+  const ramenDetail = document.querySelector('#ramen-detail')
+  ramenDetail.querySelector('.detail-image').src= ramen.image
+  ramenDetail.querySelector('.name').textContent = ramen.name
+  ramenDetail.querySelector('.restaurant').textContent=ramen.restaurant
+  
+const ratingDisplay = document.querySelector('#rating-display') 
+ratingDisplay.textContent = ramen.rating  
+
+  const commentDisplay = document.querySelector('#comment-display') 
+  commentDisplay.textContent = ramen.comment 
+
+
+  
+
 };
 
-const addSubmitListener = () => {
-  // Add code
-}
+const addSubmitListener = (e) => {
+  // // Add code
+  e.preventDefault ()  
+    const newRamen = { 
+      name: document.getElementById('new-name').value, 
+      restaurant: document.getElementById('new-restaurant').value,
+      image: document.getElementById('new-image').value,
+      rating: document.getElementById('new-rating').value,
+      comment: document.getElementById('new-comment').value,
+      id: 0
+    }
+  console.log(newRamen)
+    displayRamens([newRamen])
+};
 
-const displayRamens = () => {
-  // Add code
+
+   
+
+    
+  
+
+
+
+const displayRamens = (ramenArr) => {
+  const ramenContainer = document.querySelector('#ramen-menu')
+ 
+  ramenArr.forEach((ramenObj)=> {
+   
+    const card = document.createElement('div')
+   
+
+  
+    const img = document.createElement('img') 
+    img.src= ramenObj.image  
+    
+     
+    card.appendChild(img)  
+    ramenContainer.appendChild(card)
+
+    card.addEventListener('click',() => { 
+      handleClick(ramenObj)
+    })
+}) 
 };
 
 const main = () => {
-  // Invoke displayRamens here
+  // Invoke displayRamens here 
+  fetch("http://localhost:3000/ramens")
+    .then((resp) => resp.json())
+    .then((data) => displayRamens(data));
+  
   // Invoke addSubmitListener here
+  const form = document.querySelector("#new-ramen")
+  form.addEventListener("submit",(e) => addSubmitListener(e))
 }
 
 main()
@@ -26,4 +83,8 @@ export {
   addSubmitListener,
   handleClick,
   main,
-};
+}; 
+
+
+
+
